cbe5ba8d01a3d9535ac92baddeb2e29c7f33a036
K1ndbeetle <alx-s@bk.ru>
2023-05-31 03:49:27 +0300

    Task

--- src/main/java/EmployeeDAO.java
+++ src/main/java/EmployeeDAO.java
@@ -1,10 +1,9 @@
-import java.sql.SQLException;
 import java.util.List;
 
 public interface EmployeeDAO {
-    void addEmployee(String lastname, String firstname, String gender, int age, int city_id) throws SQLException;
-    Employee receiptEmployeeById(int id) throws SQLException;
-    List<Employee> gettingListAllEmployee() throws SQLException;
-    void changingEmployeeId(int id, String... array) throws SQLException;
-    void deleteEmployeeId(int id) throws SQLException;
+    void addEmployee(Employee employee);
+    Employee receiptEmployeeById(int id);
+    List<Employee> gettingListAllEmployee();
+    void changingEmployeeId(Employee employee, int id);
+    void deleteEmployeeId(Employee employee);
 }
